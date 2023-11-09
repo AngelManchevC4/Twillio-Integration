bcb611417d6316f90cecefc002fc02cc8f5e9499
AngelManchevC4 <angel.manchev@c4nexus.com>
2023-11-09 20:00:20 +0200

    notifaction subscribtion added, phone number prepopulated automatically, started doing the custom obj logic

--- cartridges/app_storefront_pdp_extend/cartridge/controllers/Product.js
+++ cartridges/app_storefront_pdp_extend/cartridge/controllers/Product.js
@@ -9,6 +9,7 @@ var server = require('server');
 var cache = require('*/cartridge/scripts/middleware/cache');
 var consentTracking = require('*/cartridge/scripts/middleware/consentTracking');
 var pageMetaData = require('*/cartridge/scripts/middleware/pageMetaData');
+var csrfProtection = require('*/cartridge/scripts/middleware/csrf');
 
 var base = server.extend(module.superModule);
 
@@ -22,27 +23,32 @@ var base = server.extend(module.superModule);
  * @property {String} info_selectforstock - Localized string for "Select Styles for Availability"
  */
 
-  /**
-  * Product-Show : This endpoint is called to show the details of the selected product
-  * @name Base/Product-Show
-  * @function
-  * @memberof Product
-  * @param {middleware} - cache.applyPromotionSensitiveCache
-  * @param {middleware} - consentTracking.consent
-  * @param {querystringparameter} - pid - Product ID
-  * @param {category} - non-sensitive
-  * @param {renders} - isml
-  * @param {serverfunction} - get
-  */
-server.replace('Show', cache.applyPromotionSensitiveCache, consentTracking.consent, function (req, res, next) {
+/**
+* Product-Show : This endpoint is called to show the details of the selected product
+* @name Base/Product-Show
+* @function
+* @memberof Product
+* @param {middleware} - cache.applyPromotionSensitiveCache
+* @param {middleware} - consentTracking.consent
+* @param {querystringparameter} - pid - Product ID
+* @param {category} - non-sensitive
+* @param {renders} - isml
+* @param {serverfunction} - get
+*/
+server.replace('Show', server.middleware.https, csrfProtection.generateToken, cache.applyPromotionSensitiveCache, consentTracking.consent, function (req, res, next) {
     var ContentMgr = require('dw/content/ContentMgr');
     var productHelper = require('*/cartridge/scripts/helpers/productHelpers');
     var showProductPageHelperResult = productHelper.showProductPage(req.querystring, req.pageMetaData);
     var productType = showProductPageHelperResult.product.productType;
 
-    var subscriptionForm = server.forms.getForm('subscription');
+    var customerPhone;
 
-    var customerContent=ContentMgr.getContent("QuantityNotificationMsg");
+    req.currentCustomer.profile ? customerPhone = req.currentCustomer.profile.phone : null;
+
+    var subscriptionForm = server.forms.getForm('subscription');
+    subscriptionForm.clear();
+
+    var customerContent = ContentMgr.getContent("QuantityNotificationMsg");
 
     if (!showProductPageHelperResult.product.online && productType !== 'set' && productType !== 'bundle') {
         res.setStatusCode(404);
@@ -65,12 +71,45 @@ server.replace('Show', cache.applyPromotionSensitiveCache, consentTracking.conse
                 breadcrumbs: showProductPageHelperResult.breadcrumbs,
                 canonicalUrl: showProductPageHelperResult.canonicalUrl,
                 schemaData: showProductPageHelperResult.schemaData,
-                content:customerContent,
-                subscriptionForm:subscriptionForm
+                content: customerContent,
+                subscriptionForm: subscriptionForm,
+                customerPhone: customerPhone
             });
         }
     }
     next();
 }, pageMetaData.computedPageMetaData);
+
+server.post('Subscribe', server.middleware.https, csrfProtection.validateAjaxRequest, function (req, res, next) {
+
+    var Transaction = require('dw/system/Transaction');
+    var CustomerMgr = require('dw/customer/CustomerMgr');
+    var Resource = require('dw/web/Resource');
+    var URLUtils = require('dw/web/URLUtils');
+
+    var type = "Product-Subscribe-Notification";
+
+    var CustomObjectMgr = require('dw/object/CustomObjectMgr');
+
+    var subscriptionForm = server.forms.getForm('subscription');
+
+    var subscriptionResult = CustomObjectMgr.getCustomObject(type, subscriptionForm.customer.phonenumber.value);
+
+    req.querystring.parameters;
+
+    let type = "asd";
+    var primaryKey = subscriptionForm.customer.phonenumber.value + req.querystring.parameters;
+
+    // if (subscriptionForm.valid) {
+    //     Transaction.wrap(function () {
+    //         var subscriptionEntry = CustomObjectMgr.createCustomObject(type, subscriptionForm.customer.phonenumber.value);
+    //     })
+    // }
+
+    res.redirect("Home-Show");
+
+    return next();
+
+})
 
 module.exports = server.exports();
