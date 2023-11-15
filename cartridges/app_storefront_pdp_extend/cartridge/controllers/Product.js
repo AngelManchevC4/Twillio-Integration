2b12953b9b044de90cf79b136aad30ce263f9d96
AngelManchevC4 <angel.manchev@c4nexus.com>
2023-11-15 09:34:41 +0200

    Product controller extended with append instead of replace

--- cartridges/app_storefront_pdp_extend/cartridge/controllers/Product.js
+++ cartridges/app_storefront_pdp_extend/cartridge/controllers/Product.js
@@ -35,11 +35,12 @@ var base = server.extend(module.superModule);
 * @param {renders} - isml
 * @param {serverfunction} - get
 */
-server.append('Show', server.middleware.https, csrfProtection.generateToken, cache.applyPromotionSensitiveCache, consentTracking.consent, function (req, res, next) {
+
+server.append('Show', server.middleware.https, csrfProtection.generateToken, function (req, res, next) {
+
     var ContentMgr = require('dw/content/ContentMgr');
     var productHelper = require('*/cartridge/scripts/helpers/productHelpers');
     var showProductPageHelperResult = productHelper.showProductPage(req.querystring, req.pageMetaData);
-    var productType = showProductPageHelperResult.product.productType;
 
     var customerPhone;
 
@@ -50,35 +51,12 @@ server.append('Show', server.middleware.https, csrfProtection.generateToken, cac
 
     var customerContent = ContentMgr.getContent("QuantityNotificationMsg");
 
-//do with append    
+    res.render(showProductPageHelperResult.template, {
+        customerContent: customerContent,
+        subscriptionForm: subscriptionForm,
+        customerPhone: customerPhone
+    })
 
-    if (!showProductPageHelperResult.product.online && productType !== 'set' && productType !== 'bundle') {
-        res.setStatusCode(404);
-        res.render('error/notFound');
-    } else {
-        var pageLookupResult = productHelper.getPageDesignerProductPage(showProductPageHelperResult.product);
-
-        if ((pageLookupResult.page && pageLookupResult.page.hasVisibilityRules()) || pageLookupResult.invisiblePage) {
-            // the result may be different for another user, do not cache on this level
-            // the page itself is a remote include and can still be cached
-            res.cachePeriod = 0; // eslint-disable-line no-param-reassign
-        }
-        if (pageLookupResult.page) {
-            res.page(pageLookupResult.page.ID, {}, pageLookupResult.aspectAttributes);
-        } else {
-            res.render(showProductPageHelperResult.template, {
-                product: showProductPageHelperResult.product,
-                addToCartUrl: showProductPageHelperResult.addToCartUrl,
-                resources: showProductPageHelperResult.resources,
-                breadcrumbs: showProductPageHelperResult.breadcrumbs,
-                canonicalUrl: showProductPageHelperResult.canonicalUrl,
-                schemaData: showProductPageHelperResult.schemaData,
-                content: customerContent,
-                subscriptionForm: subscriptionForm,
-                customerPhone: customerPhone
-            });
-        }
-    }
     next();
 }, pageMetaData.computedPageMetaData);
 
